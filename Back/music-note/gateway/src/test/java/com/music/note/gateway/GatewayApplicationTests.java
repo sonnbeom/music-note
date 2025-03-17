f59c1c73d49161e05906c06bff6507beff6affcc
InGyu-Moon <ansdlsrb1121@naver.com>
2025-03-17 15:11:06 +0900

    init project
    Former-commit-id: 23abe51d57068b51bcd162a2f8874d91980f00c0

---
+++ Back/music-note/gateway/src/test/java/com/music/note/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.note.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
