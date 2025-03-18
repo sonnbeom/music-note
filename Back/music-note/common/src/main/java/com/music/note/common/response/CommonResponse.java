c03b42d13523c77575bcccfa14bf2f37da695c1d
손승범 <no1beom@naver.com>
2025-03-18 15:29:03 +0900

    [BE] Feature - 공통 모듈, 게이트웨이 설정
    Former-commit-id: c48b1812434f4d9709a1c935cbca099b8ef0b442

---
+++ Back/music-note/common/src/main/java/com/music/note/common/response/CommonResponse.java
@@ -0,0 +1,16 @@
+package com.music.note.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+	private int status;
+	private String message;
+	private T data;
+}
