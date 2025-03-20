58b179b6f797ed4138f0c5d2c366ee3e21455f56
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-20 10:02:19 +0900

    feat front 프로젝트 시작
    Former-commit-id: f831797d5c73e659ab6e94700e7382735f8d4874

---
+++ Front/musicnote/eslint.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc00c7db700d03834d04a172ea709593ae5ef1bc750eb55dfc521a60d9e1baa0
+size 734
---
+++ Front/musicnote/src/App.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cb2286e66193c18f0943efb0dcecc72278cd3d92c6dd619dc3b73b73e2b9495
+size 903
