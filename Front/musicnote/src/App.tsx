cd933c979b510cd3ebbb7f9cdf44db6ea258f736
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-20 11:00:30 +0900

    feat initial setting for front
    Former-commit-id: d72b8cf05b5b674467fc034f2e724fde24b5b2fa

--- Front/musicnote/src/App.tsx
+++ Front/musicnote/src/App.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5cb2286e66193c18f0943efb0dcecc72278cd3d92c6dd619dc3b73b73e2b9495
-size 903
+import './styles/Global.css'
+import AppRoutes from './routes/AppRoutes'
+import { BrowserRouter as Router } from 'react-router-dom'
+function App() {
+  return (
+    <Router>
+      <AppRoutes/>
+    </Router>
+  )
+}
+
+export default App
--- Front/musicnote/vite.config.ts
+++ Front/musicnote/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
+import eslint from 'vite-plugin-eslint';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint({
+      fix: true, // 자동으로 ESLint 오류 수정
+    })],
+  esbuild: {
+    jsxInject: `import React from 'react'`, // JSX 사용 시 React 자동 import
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // 절대 경로 import 지원
+    },
+  },
+});
