315357644c38883b9538f68a194c0fb9d7a28677
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-20 11:10:12 +0900

    fix App Routes .jsx -> .tsx
    Former-commit-id: 6bbde722d809193951cfcd893cfdc404df5dde35

--- Front/musicnote/src/routes/AppRoutes.tsx
+++ Front/musicnote/src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
 
-export default function AppRoutes() {
+export default function AppRoutes(): React.ReactElement {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
