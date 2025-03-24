88b8b108fcb72a27f5e8f298e279cef54389cf57
민경현 <skyblue848@naver.com>
2025-03-24 12:47:27 +0900

    [FE] Feature - react query 기본 설정
    Former-commit-id: 1f18248c2c3fbcb83a3fb6ad1175360078cdd86e

---
+++ Front/musicnote/src/api/apiClient.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11c9fdcf23ef08a7b64e494a8ef243911034aae08f71195cf26d3007883e1017
+size 604
---
+++ Front/musicnote/src/hooks/useApi.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f05620bf8da0292640bcbd71afc2047785feced818c00fb5962a637e13efe43a
+size 1223
