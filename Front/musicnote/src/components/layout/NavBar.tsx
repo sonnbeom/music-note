3857dd97b675dd031451df7b73b396cf3f4515b8
민경현 <skyblue848@naver.com>
2025-03-24 14:19:43 +0900

    fix Home.tsx 오타
    Former-commit-id: bb84c36d4918bc04fdab1a62de3bc5e3bc63cd6a

--- Front/musicnote/src/components/layout/NavBar.tsx
+++ Front/musicnote/src/components/layout/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:610c6afd0f3cc27f011147eaf9d5885652cafead6e3ab0c75e9185ce09993f0b
-size 2900
+oid sha256:02c2a35c992c7466d79ad785ebd71d61afc7b20e001d31dbd9d4f1f97fbbbe71
+size 2921
