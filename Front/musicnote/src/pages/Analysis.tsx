1696a139359c9e6ff2adddd6fabb32c24863d6a4
hyjoo1226 <sparrow9576@gmail.com>
2025-03-25 14:52:57 +0900

    feat: 성향분석 radar graph 초안
    Former-commit-id: a30ecf5047a3c63ac857995cb00b52ae33aafe07

--- Front/musicnote/src/pages/Analysis.tsx
+++ Front/musicnote/src/pages/Analysis.tsx
@@ -1,3 +1,3 @@
-export default function Analysis() {
-  return <div className="text-white">Analysis</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:17827b144d0a2f1d02a659fedf48c926b7e300ef7c719bdef451c42631b3c9fe
+size 2795
