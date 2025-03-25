73787140712338e3499fe9aced848e8b284b33a4
starcraftchobo <madeinburundi@gmail.com>
2025-03-25 09:22:39 +0900

    feat : [TEST] 데이터 전처리&룰베이스 장르 추출
    Former-commit-id: c55062cb673aab57b9b956748b19caa6b74dde67

--- Data/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56e925a2d3e704a48a027ec5baefa7e050912b42b59cd2ca3a70de563a31b87e
-size 533
---
+++ Data/test/base/classification_entire.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ddccb9856687b74e8bd22cf52362f993314374279c5b53d6b111c5c56a4670b
+size 6764
