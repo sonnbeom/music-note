a092cf64ebd36ac859fd615776cf300d24d7f30a
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-27 15:43:05 +0900

    fix 불필요한 import 삭제
    Former-commit-id: 161b85845f347becd116bca4a3320239841cae16

--- Front/musicnote/src/pages/Recommendations.tsx
+++ Front/musicnote/src/pages/Recommendations.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f9b7b44ef6a2a8d0ce578d64321e43cd1706d16ee2cf8f442d3883f8917a17d
-size 6532
+oid sha256:8055b10de1f3e4980e4f4aec65ae11d2c5a8b01ba81b27418d17ab0c5704221d
+size 6470
