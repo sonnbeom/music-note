f58be9ad3084e9e0da98dc538cb16ba84919b18a
윤상흠 <kidsncar@naver.com>
2025-03-27 12:12:53 +0900

    modify : [MS] 모Schema추 추가
    Former-commit-id: c0e569464c71fbe262a5e4574aa421958f035239

--- Data/modelschemas/request_response.py
+++ Data/modelschemas/request_response.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11128f5f61252a5eb52fe1878044d0f77de09cd3f3e7d0a9dc10f22642308b33
-size 810
+oid sha256:d9830e750f7e6221b0fb0e4516f21f036db7b4cbd438792a9f5e0ab5c41670c7
+size 960
