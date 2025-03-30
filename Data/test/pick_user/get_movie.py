3514b65cc5f6761aed01b6ec76c5dcab42ae73d8
남기운 <madeinburundi@gmail.com>
2025-03-31 06:05:25 +0900

    feat: [TEST] 전처리랑 유저 영화장르 추출
    Former-commit-id: 19ea8062aad9fc4d1d0c25861ea227c14ae3466f

---
+++ Data/test/pick_user/get_movie.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60281739b3d02a210720cb5e7078db5512055d175f274471fa33f902e66ae191
+size 1564
---
+++ Data/test/pick_user/preprocess.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:25b18e8fb578730872c1b614d64207bc35652976c04f63bc76e23cea38022ef0
+size 2154
