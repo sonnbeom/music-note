5e42a716d9f681b89dfe493f3e99ebd5a66b0abb
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-31 16:27:59 +0900

    fix 백엔드 데이터 형식과 프론트 일치시킴
    Former-commit-id: 741bea50f5e57c783f1ba0e57d3d952e07650167

--- Front/musicnote/src/pages/Login.tsx
+++ Front/musicnote/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ba473dae865d8d4775bc17e5fdc57337b731fddeac4da307ab50d644de560b
-size 4814
+oid sha256:a0fbe5916a5c4c6ef2917bf6ec5946c2742ca3281aaa79b1fb83e527a8bb454a
+size 4906
