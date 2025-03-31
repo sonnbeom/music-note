6ba40ad52a6235c77a09cf1f4c01d50853395580
hyjoo1226 <sparrow9576@gmail.com>
2025-03-31 11:05:08 +0900

    feat: 취향플러스 메인페이지 구현
    Former-commit-id: 2539af61daf9602711192675b84ad34912518045

---
+++ Front/musicnote/src/features/discover/DiscoverCarousel.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd92995b7f647baa38d68fee93c9c35d3c1e834e0ee84e4e68f4beff33863c9a
+size 2192
--- Front/musicnote/src/pages/discover/Discover.tsx
+++ Front/musicnote/src/pages/discover/Discover.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dfcaa0bc5cac4f54a4d150c3aef8da9658d4c02faf41d8f6a764dd2b62d1c2dc
-size 550
+import DiscoverCarousel from "@/features/discover/DiscoverCarousel";
+
+export default function Discover() {
+  return (
+    <div className="text-white w-full">
+      <DiscoverCarousel />
+      {/* <button onClick={handleClick} className="text-white">
+        분석하기
+      </button> */}
+    </div>
+  );
+}
