8922c469f7befc6907debe5f4b04d41852abec65
hyjoo1226 <sparrow9576@gmail.com>
2025-03-31 13:28:57 +0900

    feat: 성향 트렌드 구현
    Former-commit-id: d30d10f160ce079231d511f87bb43d28d3b9e070

--- Front/musicnote/src/features/discover/DiscoverCarousel.tsx
+++ Front/musicnote/src/features/discover/DiscoverCarousel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:717635e6392905684c6e9ffec21c69d3f8e74fa8aecbe2d69b093840a02f1941
-size 2192
+oid sha256:aee1a262bd861efab334aca309466e9c6933bb98065e8252594c8f7c5a82f733
+size 2335
---
+++ Front/musicnote/src/pages/discover/LineChart.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac2a2b7ca8d2795170606f0fc9bdf4766298e2d44c81266992c6add0d22a80f1
+size 6392
