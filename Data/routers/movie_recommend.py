984266d0d3b7a63250c4372177f2a17c1233bf0d
윤상흠 <kidsncar@naver.com>
2025-03-31 21:26:38 +0900

    refactor: [SU] movie_recommend router refactoring
    Former-commit-id: 392d91e9e7b05937317d45aacda8c04b6764af63

--- Data/routers/movie_recommend.py
+++ Data/routers/movie_recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84888fb7e38d37224e64cb720a6dcca35a419b4538b40ae040e75e7f72d33378
-size 2116
+oid sha256:9b49a35d0860151ffe1c409c27261f6f0d7a51443d6db3a7610e4abc4ac88734
+size 2292
--- Data/services/movie_recommender.py
+++ Data/services/movie_recommender.py
@@ -1,5 +1,3 @@
-# services/movie_recommender.py
-from modelschemas.request_response import BigFiveScore
-
-def recommend_movie_genre(input_data: BigFiveScore) -> str:
-    pass
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3bca6facd6fcac06cfae74197fca5ee09f758956034ac9e92044d700e7da6b1
+size 2253
