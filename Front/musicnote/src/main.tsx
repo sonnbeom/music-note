e4fd2c13b9a4049e85ebbfd3244eebaff53227ae
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-03-31 17:44:48 +0900

    fe/feat/spotifyClient
    Former-commit-id: 3cec02632b8e808bfd5225ff2d07145fd5b3de9e

--- Front/musicnote/src/main.tsx
+++ Front/musicnote/src/main.tsx
@@ -1,15 +1,3 @@
-import './styles/Global.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App.tsx'
-
-const queryClient = new QueryClient()
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:d362f349fd7326385aa4a2f7f0377598a9c861faf1501851e31a84bb134180a3
+size 791
