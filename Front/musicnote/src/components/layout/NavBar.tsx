8329e37d471905c1de4fc3f98ba6160f22fd388d
민경현 <skyblue848@naver.com>
2025-03-31 15:48:39 +0900

    [FE] Fix - 반응형 웹페이지 수정
    Former-commit-id: 3712aea6cb3d96e224f1da5e3c8bbe61bbf63153

--- Front/musicnote/src/components/layout/NavBar.tsx
+++ Front/musicnote/src/components/layout/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be1c8d5d55ad69d6e1a8b1b86746c35fa2a52c0541811bc100c5a69fc0887be9
-size 3058
+oid sha256:2b07e5b7710c8370df32f6add24a1395194bfaa1291cb71dd796ce5d6a980947
+size 3072
--- Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
+++ Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed84f6cc2849ab772f93672e307b2fa374d4a39c927c158e5bed2073ce9bef6
-size 1563
+oid sha256:7a500410b0d9751d322f1721edd8e9c6e0e27bbcf73956cc008704d02377ccba
+size 1590
