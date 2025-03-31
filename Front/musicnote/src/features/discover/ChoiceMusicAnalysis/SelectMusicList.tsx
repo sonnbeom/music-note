81dc2f25e0757fb3dd1cdf0d5dd8117186e6dd00
주현호 <sparrow9576@naver.com>
2025-03-31 17:57:59 +0900

    [FE] Feature - 음악 검색 query 도입
    Former-commit-id: b869241bc16c82cf0b6a84c542146a05b01f4724

--- Front/musicnote/src/features/discover/ChoiceMusicAnalysis/SelectMusicList.tsx
+++ Front/musicnote/src/features/discover/ChoiceMusicAnalysis/SelectMusicList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0293389f143530117fffa4290eba057bb123d6206bcaf1b18cfb7c8d89598fa
-size 2239
+oid sha256:f611cfcde1a9c8b6c9f5e63721d00223e3fa831968e6f609a19fb782341fc7d9
+size 2268
