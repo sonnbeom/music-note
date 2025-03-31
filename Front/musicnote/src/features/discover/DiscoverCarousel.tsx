45e363c015dae648d7859c155c0726322c4855c4
hyjoo1226 <sparrow9576@gmail.com>
2025-03-31 13:40:59 +0900

    style: 성향 트렌드 캐러셀 이미지 추가
    Former-commit-id: 17d3119ddbb2327278e98e1ad2456a8e465b1cbc

--- Front/musicnote/src/features/discover/DiscoverCarousel.tsx
+++ Front/musicnote/src/features/discover/DiscoverCarousel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aee1a262bd861efab334aca309466e9c6933bb98065e8252594c8f7c5a82f733
-size 2335
+oid sha256:44970f9149e70fd6bd0c06e684c8f94614da2e7f1a10a4172e4cb0b10562d47b
+size 2419
--- Front/musicnote/src/pages/discover/LineChart.tsx
+++ Front/musicnote/src/pages/discover/LineChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac2a2b7ca8d2795170606f0fc9bdf4766298e2d44c81266992c6add0d22a80f1
+oid sha256:0f6730a41ba4dc03a1d9819e6635c98ac944caa3ea1c58352e340598735bfc38
 size 6392
