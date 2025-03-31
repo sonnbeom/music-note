1e22ebec42ecf526c3411b873408d9cf923de31c
남기운 <madeinburundi@gmail.com>
2025-03-31 20:28:33 +0900

    feat : [TEST] 추출된 장르로 영화뽑기
    Former-commit-id: 662360f7d77c512cdf2bb870f88fee3202b641dd

--- Data/test/tmdb/api_request.py
+++ Data/test/tmdb/api_request.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10645a1f71b590de3aa610bd77bddde9a028f0db4cbebb5064cefff0e601f8d3
-size 13503
+oid sha256:d00d4502f37090f0c7196bd1e86147042a2b2ac83950ee9f9bcc3b71f16a3804
+size 2172
