8ddf5d9abaf1a42a2f76f11bf444949771def426
윤상흠 <kidsncar@naver.com>
2025-04-01 09:47:13 +0900

    fix: [SC] remove deploy scripts (managed on EC2 only)
    Former-commit-id: 068c7152c130c3565072ac1d395b5c0359e0d013

--- Data/fastapi-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🚀 fastapi-server 배포 시작"
-
-docker-compose -f ./Data/docker-compose.yml pull
-docker-compose -f ./Data/docker-compose.yml down
-docker-compose -f ./Data/docker-compose.yml up -d --build
-
-echo "✅ fastapi-server 배포 완료!"
