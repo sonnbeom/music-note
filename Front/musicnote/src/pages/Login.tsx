3db25a66df61a9a34223905a459f99aa5c5fc7a5
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-01 15:50:54 +0900

    fix 홈화면 렌더링 시 정렬 이슈 해결
    Former-commit-id: 09052c098f6968884231053898877982be09e549

--- Front/musicnote/src/pages/Login.tsx
+++ Front/musicnote/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c18bf11e39e33479a3a854b7697fb800e35aba5cecbd0e3ae77a94e1103e8a6
-size 4835
+oid sha256:b7686808414ff1e96df2e471c3d0e4e207e26d791569577491c1bf5eb01a6d44
+size 4793
