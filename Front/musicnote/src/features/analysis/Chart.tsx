59c54d59f1f229c2936546db3d37365c05045126
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-01 15:20:38 +0900

    modify calendar, main page
    Former-commit-id: c6e682e95ee695611cf3a6b69f9e487ea7aab6b9

--- Front/musicnote/src/features/analysis/Chart.tsx
+++ Front/musicnote/src/features/analysis/Chart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b670b2f04e3c8d3228b845f18ab3fe3cd28ef5ef59f211b7ae1da8f7f061eb0d
-size 2837
+oid sha256:1a5b68daf8c1099dafef83fdfbee9d205cf22d20a7836768caf153f0893390ba
+size 2939
