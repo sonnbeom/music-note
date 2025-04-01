651511ef9ec00585870e19de89f333567731c8cb
민경현 <skyblue848@naver.com>
2025-04-01 15:23:42 +0900

    [FE] Modify - 렌더링 방식 변경
    Former-commit-id: 575b8555596d01ecb21f649d9ee0841db8610c97

--- Front/musicnote/src/pages/discover/Discover.tsx
+++ Front/musicnote/src/pages/discover/Discover.tsx
@@ -2,7 +2,7 @@ import DiscoverCarousel from "@/features/discover/DiscoverCarousel";
 
 export default function Discover() {
   return (
-    <div className="text-white w-full">
+    <div className="flex flex-col h-[calc(100vh-80px)] items-center justify-center text-white w-full">
       <DiscoverCarousel />
     </div>
   );
