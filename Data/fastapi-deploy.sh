c6b9726240f8814783b6db8c55c6e21cf3b6d47a
윤상흠 <kidsncar@naver.com>
2025-04-02 12:08:46 +0900

    fix: fastapi-deploy command 3rd
    Former-commit-id: 717cdfbf858862e566c0834d905e3fbcf4b13c42

--- Data/fastapi-deploy.sh
+++ Data/fastapi-deploy.sh
@@ -3,12 +3,12 @@
 echo "🚀 fastapi-server 배포 시작"
 
 # 1. 기존 컨테이너 종료 및 제거
-docker compose --file docker-compose.fastapi.yml down || true
+docker-compose --file docker-compose.fastapi.yml down || true
 
 # 2. 최신 이미지 pull
 docker pull ysh933/fastapi-server:latest
 
 # 3. 새 컨테이너 실행
-docker compose --file docker-compose.fastapi.yml up -d --remove-orphans
+docker-compose --file docker-compose.fastapi.yml up -d --remove-orphans
 
 echo "✅ fastapi-server 배포 완료!"
