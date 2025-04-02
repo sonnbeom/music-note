d34298ce01306f4a254e0bc8b12d097484034c35
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-02 14:17:31 +0900

    fix: 주소 수정 및 디테일 모달에서 아래 패딩이 모자라 내용이 나오지 않는 오류 수정
    Former-commit-id: e8325e519d5fd22f79cceaee1ff8b58af1ec6931

--- Front/musicnote/src/pages/recommend/MyRecommendationDetail.tsx
+++ Front/musicnote/src/pages/recommend/MyRecommendationDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02a23075edd5366b40ab609631d737cc48723ff6aa1d5df861bf981233932f53
-size 4493
+oid sha256:e3864f18d8124da110e1f36eadae722ec618e7044b17f24491246a8eb8906eaf
+size 4753
