774c25eb262ccfd6819138b1b2288da2e0208245
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-02 15:05:52 +0900

    fix: 리포트 공유 시 폰트가 적용이 안되는 문제 수정
    Former-commit-id: b50acb37ae9a070c51bd0d9dfd69765d96973286

--- Front/musicnote/src/pages/analysis/Report.tsx
+++ Front/musicnote/src/pages/analysis/Report.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f1bfea1376ece1784be0fd2be35eb43fe9e94c271ade0507879efd03024fa24
-size 4485
+oid sha256:c8b20dc36f99f0b32ee81a89f1411b560bbef325f283eb0251438d0615c969f9
+size 4674
