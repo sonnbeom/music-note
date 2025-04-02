d8fae0daf106601dda7002801d431eababa3192b
남기운 <madeinburundi@gmail.com>
2025-04-02 12:40:23 +0900

    feat : [TEST] 유저 클러스터링과 클러스터링 내 유저 랜덤 선택
    Former-commit-id: ec260efbcb543ed44181249399242e54ac4fc09a

---
+++ Data/test/pick_user/bigfive_preprocess.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:170c6af8058be968e423ebc3524023baf7844ca12902a566d7fa3ee7cc4ad036
+size 655
--- Data/test/pick_user/preprocess.py
+++ Data/test/pick_user/preprocess.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2832a409f2d8eb3b8376ff1c076a3ae8f236d0a09e9f237a07ff7d6ef5e064ad
-size 2201
+oid sha256:ab540e4f3be5f15be0166dc0c0cd9a38512312a047241b5590396e252b153a31
+size 2148
