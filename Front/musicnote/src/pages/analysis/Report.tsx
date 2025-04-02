ccc5e44e7dd58e3f6ec50f538c5cc1aa15c841bd
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-02 17:21:03 +0900

    fix: 공유 버튼 누를 시 font가 처음에 불려와지지 않는 문제 수정
    Former-commit-id: 634eb05a079b18c50f632afef259593405de3f15

--- Front/musicnote/src/pages/analysis/Report.tsx
+++ Front/musicnote/src/pages/analysis/Report.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fec5da2871db24b8e43c01e89cce9012068819eb6afe53c45a0a593a2138466
-size 4958
+oid sha256:a2a5f8b22fd93f9ff8edaf5c8d43a15ba3e4b478cb1ce6de0fc8e7ca96220155
+size 5206
