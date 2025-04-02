bc8ca72f8efef8f486287502348c29473938318f
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-02 15:52:22 +0900

    feat: front PWA 설정
    Former-commit-id: 8c4b2bc119ac5703e02971a2348682e1b2575562

---
+++ Front/musicnote/src/notification.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3f40b23002b6618bc1f14ef44e02a195846051d8c7d90ccee07fbd5d472fa37
+size 909
--- Front/musicnote/vite.config.ts
+++ Front/musicnote/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a769399390b44fa71ca9a0a9d477db9e43a656cc0f7d58c3af8b44a426c4ab9a
-size 627
+oid sha256:7a69e95ac9a93f543e3cde80fea517d7a24ee3d832f4999e035a426563e58db5
+size 2632
