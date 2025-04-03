021c1d0802e76f9ffc0b8d5f44398563f5777214
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-03 13:41:13 +0900

    style: line Graph 담는 div 스타일 수정
    Former-commit-id: daf44adc6a371ae28521e51366f7754028f09c10

--- Front/musicnote/src/pages/discover/LineChart.tsx
+++ Front/musicnote/src/pages/discover/LineChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bf5ed18f522908149c39d407874f59cc07288604a4f12a903a61ad07fd72498
-size 7274
+oid sha256:f3351ffc74e9473fc4a3b9fe746e1dd02ebb803b4a775150e203b41ea45c8685
+size 7424
