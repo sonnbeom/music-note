be5f2151680ab5dbfde802ea273d1ac058e20140
윤상흠 <kidsncar@naver.com>
2025-04-03 15:20:59 +0900

    fix: main.py and requirements.txt
    Former-commit-id: 6ebfb1aa29ef5580f78bc6b9a2e087ede72f97a2

--- Data/main.py
+++ Data/main.py
@@ -3,6 +3,11 @@
 from fastapi import FastAPI
 from routers import router as main_router
 
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
 app = FastAPI(title="성격 기반 추천 API")
 
 # 통합 라우터 등록
