a07f9c414cb8acecc3a68574e808f3247d829031
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-04 13:41:27 +0900

    fix: favicons 주소 상대경로로 수정
    Former-commit-id: f899179394a446f3f042238e4cdbf37b17a93b0f

--- Front/musicnote/vite.config.ts
+++ Front/musicnote/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deacaba14345ea7b8096f6087626752383d134df8b4cee3642405b25b25fa0d2
-size 2628
+oid sha256:34051f44f7974c2b0c9a59f3f76927da181ca60f7f88b265f01cf50ec0437d90
+size 2632
