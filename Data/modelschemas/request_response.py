ff0b97795f2ee83fef513c844cf455af04d146d1
윤상흠 <kidsncar@naver.com>
2025-04-04 10:02:04 +0900

    fix: [SC] fix return schema
    Former-commit-id: bcc72efa9a46510e1097dd963d0dca66f1a0090c

--- Data/modelschemas/request_response.py
+++ Data/modelschemas/request_response.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba904fa28768db2e05727b28c59ab7b67ede77e5b0f8713b39919db126120e24
-size 1950
+oid sha256:bf4c01035df9d288610fb02c7c68d0faf3710009f4dd19b367f8f20e1868af3b
+size 2221
