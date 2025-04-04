58b43c1196ec19fca286968705816c1d8b360732
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-04 14:14:56 +0900

    modify: spotify access token 이름값 변경
    Former-commit-id: 2a504d45838007154ba070beaec73f3a34070da7

--- Front/musicnote/src/api/apiClient.ts
+++ Front/musicnote/src/api/apiClient.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1584a549c3eb8e66c303ec397e35f1bf86f2f66c64a4a4786c589b0739f8d427
-size 2506
+oid sha256:a529255dbd28f64507396e3db013946dee3beec30a4d9801eb3f4427e5aa6ce5
+size 2508
