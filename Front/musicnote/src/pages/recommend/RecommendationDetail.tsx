f2c9f3d45522e6126f015626cac3589192cc0f64
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-04 13:10:36 +0900

    modify: NavBar에 따른 패딩 추가 이유로 페이지들 스타일 수정
    Former-commit-id: 369af70fe12e6f7ea2d9587ea5b5f835700c75d5

--- Front/musicnote/src/pages/recommend/RecommendationDetail.tsx
+++ Front/musicnote/src/pages/recommend/RecommendationDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:934755b051179e09a6f3a941d2b81da9f5234cf6950fcb108ecc71e83e52a09a
-size 16334
+oid sha256:497d4f7adb3a5ccc54a8354ab8c9fcacd3f3d561ded23d018978fde4f1cabfeb
+size 16332
