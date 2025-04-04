8c6139f4045f4ae07619353c58e6c7b361443e61
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-04 16:18:36 +0900

    feat: add extra log
    Former-commit-id: d421269adb75b264f3cf4152eab33c07cc49a450

--- Back/music-note/music-type-service/src/main/java/com/music/note/musictype/service/kafka/consumer/RequestEventConsumer.java
+++ Back/music-note/music-type-service/src/main/java/com/music/note/musictype/service/kafka/consumer/RequestEventConsumer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61487c2f1b5e27936cbb7f522418be45c1c88501e7408cc559028578029d411f
-size 777
+oid sha256:2be4dfe12a467a9a49d8ad815886e87c29372619eedd56f6c56b8693dc4ce3d0
+size 789
--- Back/music-note/music-type-service/src/main/java/com/music/note/musictype/service/kafka/producer/TypeEventProducer.java
+++ Back/music-note/music-type-service/src/main/java/com/music/note/musictype/service/kafka/producer/TypeEventProducer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06714145f21abfc2e5737402a7647b7d38b0859a78381c6759e55f42c6d2bd71
-size 659
+oid sha256:596972e53fe7a5a3c9b1b1323ce99c1330e084237e56bb1299b094144ec1beee
+size 671
