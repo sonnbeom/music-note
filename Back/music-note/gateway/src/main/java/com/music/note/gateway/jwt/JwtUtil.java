a2c61e4afd75ac6832902c2462bb5828dd5598e7
InGyu Moon <ansdlsrb1121@naver.com>
2025-04-05 22:57:58 +0900

    feat: check Authorization token in gateway
    Former-commit-id: 905ef457918d25533a3b0b4771a2d00ffb37d05f

---
+++ Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08345f75df4b19dbd4e05880bac5a05483f7bd58cdc11d9c94be74b60f5192c5
+size 1088
--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8e5198657243b24ca5baa65d7853a4d85ac115f087d31b2da1d5e8f365d83ce
-size 962
+oid sha256:1fa0a73be7697b5802db27ef782cbcbc488349044fe0230eb648f1a056e2ae5e
+size 1232
