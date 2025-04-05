8214fd2f57fc22579758227c6e709e73c108aae9
손승범 <no1beom@naver.com>
2025-04-06 06:59:44 +0900

    [BE] Prod - 추천 서버 데일리리포트 저장 시간 롤백 적용
    Former-commit-id: f6f8fe3d33f861fdac8e46e945b27639e10371e4

--- Back/music-note/type-domain/src/main/java/com/music/note/typedomain/domain/PersonalityReport.java
+++ Back/music-note/type-domain/src/main/java/com/music/note/typedomain/domain/PersonalityReport.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b6e1a34b91365b7e50ccf01261c5fe7555a810825afef84dbff586833b0e917
-size 1318
+oid sha256:335ae7f981d555a772a52e9dd7555d21950928816f95f7bbe786c1be6cf88683
+size 1368
