ceb850c0bd12eda2059fa9aabf3bf4ec2950b03f
손승범 <no1beom@naver.com>
2025-04-05 10:58:22 +0900

    [BE] Fix- build 에러 해결
    Former-commit-id: 241e5c1bd722462d7f220916b722759f5bdac425

--- Back/music-note/recommend/src/main/java/com/music/note/recommend/mapper/report/ReportMapper.java
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/mapper/report/ReportMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c03c408c9e28abab0c2860bd1b58d65830591287dda90b6f075ce54715c63e17
-size 2358
+oid sha256:d600df0b43c19f6e8697e1b50fb8a0a55bdddd8e42449eb83a2bb4706a7a8061
+size 2394
