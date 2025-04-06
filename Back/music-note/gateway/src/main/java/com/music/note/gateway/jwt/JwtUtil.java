2926f192d36d45d71f69959f6bc58dc3cf59b719
InGyu Moon <ansdlsrb1121@naver.com>
2025-04-06 18:04:51 +0900

    fix: change sign key code
    Former-commit-id: 44f562c7b2cee43fdb27c7b2f4f4d737e2821522

--- Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
+++ Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08345f75df4b19dbd4e05880bac5a05483f7bd58cdc11d9c94be74b60f5192c5
-size 1088
+oid sha256:dcf15e7867bc86da166712bf8d2e4163fc1dbbd92b68920beaffab95f3bd169a
+size 998
