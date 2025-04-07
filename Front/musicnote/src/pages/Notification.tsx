85ec2a3858c425b16d3be8da9043c59fe1be6a0d
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-07 16:46:43 +0900

    fix: 주간 리포트 요청 오타 수정
    Former-commit-id: 4ad8dbb8b2e8f294ee7a1dc6d54071aa55d9a6e0

--- Front/musicnote/src/pages/Notification.tsx
+++ Front/musicnote/src/pages/Notification.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43c1b487a9f30d54da12fdf9b5f7f09e608e6ed019bb503adefee3231982e0ad
-size 6199
+oid sha256:9ecdbe82a04a9a207b4d1e81976eb8e18019b483c0475c9a4e328417e481d4e9
+size 6194
