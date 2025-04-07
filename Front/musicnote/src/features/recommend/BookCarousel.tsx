a4d36e4336dd40e5be02360e3bb38e5788bb3268
민경현 <skyblue848@naver.com>
2025-04-07 10:28:39 +0900

    [FE] Feature - 추천 영화, 음악, 책 API 연결
    Former-commit-id: 661cadab2a4fdc08da99edeb7a3e62c0118d8aee

---
+++ Front/musicnote/src/features/recommend/BookCarousel.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4d9d8abb9f719971e1053cacc7906b871778f576af33e7435f0ae8031b1ff8e
+size 1875
---
+++ Front/musicnote/src/pages/recommend/RecommendationBook.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea244bfbb7ef8079db3c4586f92fd4c7f78b1b8e7cfb48efbd36c7411de01b3e
+size 15608
