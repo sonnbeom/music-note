084e81c4355111c2a9205ec407b5f474fbf8bc14
hyjoo1226 <sparrow9576@gmail.com>
2025-04-07 10:42:25 +0900

    fix: 성향분석 페이지 진입 시 초기값 설정
    Former-commit-id: ffc9ae0a0f803cbbbf3349c83f3cae2d1113f9cc

--- Front/musicnote/src/features/analysis/Calendar.tsx
+++ Front/musicnote/src/features/analysis/Calendar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e26c480cda488828e1eb0a29d5ea1084f4e51047ea94fbc1446db22e943dac
-size 6108
+oid sha256:06864c8848504eead55765ce8de23178e4cd5ac39c48ab6459cda3169bba659c
+size 6136
--- Front/musicnote/src/pages/analysis/Analysis.tsx
+++ Front/musicnote/src/pages/analysis/Analysis.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ba06153b55e2b53f225ca9f87ff7b85a50dbba9336e150ddf16174f1395cca8
-size 12223
+oid sha256:8d0b056e3428df081806ef9efef12fae704e56fe808d0926ceddbfd14866a175
+size 10952
