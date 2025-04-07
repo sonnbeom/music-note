da840ac73b8a70d0dac8e3c1194846b8860a35a5
윤상흠 <kidsncar@naver.com>
2025-04-07 11:23:29 +0900

    modify: [utils] modify bookrecommender
    Former-commit-id: 92e79fe6c390b18878557fbb09b5da7b267f39ca

--- Data/utils/recommender/book_recommender.py
+++ Data/utils/recommender/book_recommender.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb9bb04b9b8115bc29da16ac8165f0c98153ca46a82797ef5635a351fb4c79a
-size 4166
+oid sha256:39bbde08531cb3051c239eb513ad7b9b61a2ccc6c2bba2babbd09ac553ab66a0
+size 5687
