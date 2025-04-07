56308f5af0a0ca41fc4bfe6a6c657edcdd80d69b
남기운 <madeinburundi@gmail.com>
2025-04-07 14:25:34 +0900

    fix : [ms] Music모델 변경
    Former-commit-id: 7551c3431bdd485d7b47d8cd4f01855d4afc6b57

--- Data/modelschemas/request_response.py
+++ Data/modelschemas/request_response.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eca408ac0a3e7733d7093190ef82088750e8e404c14189e4299de0613489d536
-size 2543
+oid sha256:3b9a968ab6053858c5dbf4cff8ef8ff3030ecdc647a907192cf60658617d02b3
+size 2565
