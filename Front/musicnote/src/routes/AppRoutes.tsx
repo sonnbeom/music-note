0d28150a31997b16995cd76d99abc7f6d0101cdc
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-07 13:47:56 +0900

    fix: private route 적용하는 과정에서 route 오류 난 것 수정
    Former-commit-id: 4bf87a9f51934098941529b72ca1547cc396fa62

--- Front/musicnote/src/routes/AppRoutes.tsx
+++ Front/musicnote/src/routes/AppRoutes.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e72f203565e59b716885dd43460920c26a47e5972786c7ed044a9b81cdfa81d
-size 2634
+oid sha256:212ad6a1eade27525e1fbf374668abc5be4b7b2bd06bc8732027bc24316d1cbd
+size 2741
