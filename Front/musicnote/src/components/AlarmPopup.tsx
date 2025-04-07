6e770de28f891376909bba22d5edf0634479fb07
hyjoo1226 <sparrow9576@gmail.com>
2025-04-07 13:54:17 +0900

    알림 팝업 임시저장
    Former-commit-id: c7c9f54d33a091c69b197e8da0b5c727b0d32f97

---
+++ Front/musicnote/src/components/AlarmPopup.tsx
@@ -0,0 +1,8 @@
+// 칠드런 방법이냐, show 인자냐
+export default function AlarmPopup() {
+  return (
+    <div className="w-full h-[60px] bg-main text-white rounded-lg">
+      <p>🔔성향 분석 보고서가 도착했어요. 지금 바로 확인해보세요.</p>
+    </div>
+  );
+}
