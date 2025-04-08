795228e858c4f0b873fd5230e02c5eb2c6519a7b
윤상흠 <kidsncar@naver.com>
2025-04-08 13:10:48 +0900

    fix: generate todayquote
    Former-commit-id: 1dde4175af72652479e08fd79942d78acf50f6e8

--- Data/routers/generate/report_generate.py
+++ Data/routers/generate/report_generate.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6ea138f62f62293932e56827993fa813503a7ff2e0024109fc4ede1feb47b99
-size 606
+oid sha256:7d6153b2e295f3450e0914fe362738d785e710f8ceebfc9ab7f3638b262ad447
+size 587
