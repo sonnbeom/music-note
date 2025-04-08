7417a1a2d1d5523d47718293392d72dea2bbc579
윤상흠 <kidsncar@naver.com>
2025-04-08 13:00:05 +0900

    feat: add todays quote
    Former-commit-id: 51231cce0a430db6757f9d72664723b73cc7dc84

--- Data/utils/generator/report_generator_v2.py
+++ Data/utils/generator/report_generator_v2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a94f3edfd010a272afd9753aefe48d610bdbeb25eed4ecca491601724aed5e6
-size 6964
+oid sha256:3f5ad02ae891db0c8780205a491b7be1cc418410c113142ebd0e4ce9388537f2
+size 8437
