d9bdb342491e221ec85d52ef05ca6dd16e907fea
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 13:07:06 +0900

    feat: change daily url
    Former-commit-id: dff59231e10872954f40dc5bc64ff4f505d833d2

--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2297fcec80a3dd0b44d8c94a5fa8809927bf6f8aa9efa499fb956e9b6f649736
-size 2219
+oid sha256:5d6d449d3e3882bab90f01ce3a0dabc68ee008bbe71ed785d9c599200946bb75
+size 2213
