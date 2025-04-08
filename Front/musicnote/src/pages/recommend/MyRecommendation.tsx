a7e07bb2ced5c5a5c9b019081056c4fddf758960
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-08 13:37:33 +0900

    feat: 추천 보관함 api 연결
    Former-commit-id: adc743fdabbe4fc6447c6d1af27b8bb7d8fc8e27

--- Front/musicnote/src/pages/recommend/MyRecommendation.tsx
+++ Front/musicnote/src/pages/recommend/MyRecommendation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2490f63ea5698fc9db8a3198cefe45a09657811b9b109eaeeb161951b6b2a1b9
-size 10806
+oid sha256:fec533a14f07a681b3850841c9c1398976ab8365f63c833f66d172daac930b8f
+size 10570
