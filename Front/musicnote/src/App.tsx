49855f0fbca4aaa530e33238aa7ecd16b54a972e
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-08 14:05:41 +0900

    fix: subscribe 시 spotify AccessToken 집어넣기
    Former-commit-id: 59f1095213ec43693ecdc85b4c5a4972954719c4

--- Front/musicnote/src/App.tsx
+++ Front/musicnote/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5b814c89424d170a485eb10945829ae60542587cc46fc7bcabab29f9970a13a
-size 8110
+oid sha256:aca88b2bd3bd25b733ddf48915f1416f4c5d3b892391ee10b9a41940b4cbfb2d
+size 8116
