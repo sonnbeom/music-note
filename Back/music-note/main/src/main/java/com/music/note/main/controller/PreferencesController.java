62d320caaa1450cd7d40be134b63569c67854cf2
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 13:11:41 +0900

    feat: add log message
    Former-commit-id: fb5bcd0e14021246ffe8e66da4ea3a1540a8c014

--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d6d449d3e3882bab90f01ce3a0dabc68ee008bbe71ed785d9c599200946bb75
-size 2213
+oid sha256:0a36ce9ad424251cb6f03fd791b7672d429bf0051841f9d548213395b9fd4c3e
+size 2305
