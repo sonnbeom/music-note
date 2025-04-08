62094250e7713e6d12e6800d48a7287ae8bafb73
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 15:42:18 +0900

    feat: resolve problem
    Former-commit-id: b1523e5d0a59865d1cd564d855bba8385369c536

--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64afe0b32bb7235277d2083dd2b7b987988ce6d764ce7e5914121ca4595e4def
-size 2959
+oid sha256:cb1a1760670fb2c6edeb26cfcfb102643956c21afe2a9e5b5ffa4aca94eb65fc
+size 2397
