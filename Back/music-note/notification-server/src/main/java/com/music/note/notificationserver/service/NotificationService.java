bf5bbbfac96208d5f53880fc7a7970abb451f2cf
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 17:02:47 +0900

    feat: add notification log
    Former-commit-id: 427ef059180eccfdb544d94e2ad8267e03a09645

--- Back/music-note/notification-server/src/main/java/com/music/note/notificationserver/service/NotificationService.java
+++ Back/music-note/notification-server/src/main/java/com/music/note/notificationserver/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d52999977bef43ec37c3ed77fe803b6558f35a70be8155865e77f5f23990fac
-size 2356
+oid sha256:de6bf25d494d51774d697d52421975c1e5e43d93990bae1aa03d4dbf5e646e5f
+size 2489
