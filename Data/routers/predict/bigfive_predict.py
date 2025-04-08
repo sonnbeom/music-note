dcc16337f0563abe2bba5e01d7813da713a67acd
윤상흠 <kidsncar@naver.com>
2025-04-08 15:07:58 +0900

    fix: [Routers] generate korean keywords
    Former-commit-id: fabc1e52c4e91123ec900ea7d3fc0e5f4872ac11

--- Data/routers/predict/bigfive_predict.py
+++ Data/routers/predict/bigfive_predict.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:699f1707cc2006b2a6d755964ec36b6690ef1032b2ecedac644bff038b63f33a
-size 1031
+oid sha256:f0b1fe5109d3cf822256191ff00eaf2dc04b3e21cfcee4b898adaf8e0329df0c
+size 1046
