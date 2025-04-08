1c08f68cffb177aa71577e8da594c7e6d26ecf7f
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 12:56:02 +0900

    fix: change log message to english
    Former-commit-id: 630fd956129c50279a54611f199e5d96f4a6a82b

--- Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
+++ Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcf15e7867bc86da166712bf8d2e4163fc1dbbd92b68920beaffab95f3bd169a
-size 998
+oid sha256:761b88d8bfb10469b0770a4dfb4ac9f4e3a522bc39b909a6353b4715298c6437
+size 1151
