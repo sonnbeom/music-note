400115b927cd462a0ac41f5be213a60e4097e157
손승범 <no1beom@naver.com>
2025-04-08 13:19:16 +0900

    [BE] Feature - 홈 화면 데이터  응답 구현
    Former-commit-id: 37b9041ac62fdd32c2555da3d716702f909a44d3

--- Back/music-note/recommend/src/main/java/com/music/note/recommend/mapper/report/ReportMapper.java
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/mapper/report/ReportMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91fd639481415dc7a784f471a84b43eac246e2399b801f03feebd0f74205e4f7
-size 2972
+oid sha256:9b78203d828ba19406d20bbd96e0f71e8f90568b617e3e71533aea76fbd14564
+size 3340
