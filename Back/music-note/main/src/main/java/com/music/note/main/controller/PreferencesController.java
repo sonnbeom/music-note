d1d619a022169b251812a5d43be6692775d601ba
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-08 14:06:06 +0900

    feat: add token header
    Former-commit-id: 713e91a44583556a56cc5d2a1f4aed77727ed61c

--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d1a7738b8bea18fd3146f6bc8d457915b66a1374e204d3eb7b8f04008084932
-size 2366
+oid sha256:bcc0f1a6b79f76a7205fff315ce873cca9e2cda8727d5be74d8fff7790c65010
+size 2418
