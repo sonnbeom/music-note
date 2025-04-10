96d635c52cbfb54cd148b50b1bb37798c93ecdef
hyjoo1226 <sparrow9576@gmail.com>
2025-04-10 18:08:35 +0900

    수동분석 임시저장
    Former-commit-id: ea5df4ea92e62a79a6ad03a0daa6d52b0d1b7ba8

--- Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
+++ Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87e71ae4df46c0dde0bf7d1c6b2d89062f5b21f1735275833f8c13a0c2844c75
-size 4998
+oid sha256:8c78d6e070decb601e553eddac17a8cf67dbd4e1d5f66b17911ca829cae44ca1
+size 4815
--- Front/musicnote/src/pages/discover/ChoiceMusicReports.tsx
+++ Front/musicnote/src/pages/discover/ChoiceMusicReports.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3c0b48b81e5542ac6e8261a09ce8c1cdd760ea24f2067fc4b20f1130d77497c
-size 4565
+oid sha256:ffef0472da926d93fbc4ae9140463c0a5fd545a99c71d238e7cd17c841391bba
+size 4664
