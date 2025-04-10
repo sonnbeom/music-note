094308c11b5be119fca0e6afd79188eee1a7699f
윤상흠 <kidsncar@naver.com>
2025-04-10 15:40:45 +0900

    modify: [SU] book_recommend.py add randomseed
    Former-commit-id: 91fb2b202d59c8ed3e4809c295ec3ca281852261

--- Data/utils/recommender/book_recommender.py
+++ Data/utils/recommender/book_recommender.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:263ff6305f0fd1c299664749379cc8226b0e7456c54878f596a923c1e5ad9d60
-size 6124
+oid sha256:bc6de96f231d99ea5d67a51382a17dfdb994004af038a39f40e88c6e82f49b09
+size 6082
