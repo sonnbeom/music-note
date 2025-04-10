eb752474d1bbace38aadfdc98c428b57f469d39a
손승범 <no1beom@naver.com>
2025-04-10 10:45:52 +0900

    [BE] Feat 추천 서버 logging 데이터 추가
    Former-commit-id: c74e130f68ba9be8649c59a2c6ed67835a69e169

---
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/common/logging/dto/LogEvent.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a9f5dac33deafd2300d2e74c3697543cd4f4d068c02b505931c096b2a969a00
+size 946
---
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/constant/log/report/ReportConstantAction.java
@@ -0,0 +1,8 @@
+package com.music.note.recommend.constant.log.report;
+
+public class ReportConstantAction {
+	public static String READ_REPORT_MONTHLY = "read_report_monthly";
+	public static String READ_REPORT_DAILY = "read_report_daily";
+	public static String READ_REPORT_TREND = "read_report_trend";
+	public static String READ_REPORT_MUSIC = "read_report_music";
+}
