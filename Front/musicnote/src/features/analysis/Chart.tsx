7daa68b6ae0ab4140a87e36a48198d28eaca1882
hyjoo1226 <sparrow9576@gmail.com>
2025-04-11 08:49:39 +0900

    fix: 레이더차트 정규값 사용
    Former-commit-id: b4c0e25fbf03a804f4b1264ac7c4097b330bac6b

--- Front/musicnote/src/features/analysis/Chart.tsx
+++ Front/musicnote/src/features/analysis/Chart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a5b68daf8c1099dafef83fdfbee9d205cf22d20a7836768caf153f0893390ba
-size 2939
+oid sha256:42af8c94abef92a6d7101dc606b3645b6655de31d55e424344828b216c378d58
+size 3216
