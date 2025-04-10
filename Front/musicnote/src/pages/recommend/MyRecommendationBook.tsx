cd9283c55fdca49ab50bb479dd92ad540b991c51
Owhen Min <thwn911@gmail.com>
2025-04-11 04:20:37 +0900

    fix: 추천 책 보관함에서 최신순으로 정렬되도록 수정
    Former-commit-id: e28a5e525a3dd9b6bbbfa97b481752c7cc7c6b71

--- Front/musicnote/src/pages/recommend/MyRecommendationBook.tsx
+++ Front/musicnote/src/pages/recommend/MyRecommendationBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d538c4826ef0dbad8c4c076f56870e2e4dada98306c77fbdc198d09b24e9c683
-size 4574
+oid sha256:4f0503e97c3d88b6c46b300b25a5008777fb99f5760261d1f5b1e611912502f3
+size 4584
