268e522e91c4130a04bc2a7a146f46de04568674
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-10 11:42:09 +0900

    fix: 오류로 인해 PWA 설정 해제
    Former-commit-id: 9a5439a663238cb10e932ee0bbd14caeb41597c1

--- Front/musicnote/src/App.tsx
+++ Front/musicnote/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01a40ec6ff55de2017c67ccb8ea1aaae452897a3b2df1e91f913bbe8ca35311
-size 9809
+oid sha256:313bbf8892e7d60d55925e08f342cb49f3419a07481d2fa5d88bd6fc1f1a7ad1
+size 10684
