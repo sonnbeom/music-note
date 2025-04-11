bcf15e4cfb57abfe6a24c901095717636865f562
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-11 09:09:10 +0900

    fix: resolve error
    Former-commit-id: 7848e7b0b189907f4edc2cf3caf3fa06451ce13b

--- Back/music-note/music-type-consumer/src/main/java/com/music/note/musictype/consumer/service/DailyReportService.java
+++ Back/music-note/music-type-consumer/src/main/java/com/music/note/musictype/consumer/service/DailyReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebda1c68873ed8df2f376acdd7005c930eead3f94e56194750fc9e0ef821e188
-size 4917
+oid sha256:76a6d43845a90d7536e87e0f21807a60cd65a40979e2e1f2648755c69124d940
+size 4302
