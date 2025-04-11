4c5cfdf027312141c100677f4184987a8b21dc36
hyjoo1226 <sparrow9576@gmail.com>
2025-04-11 09:09:50 +0900

    fix: 책 추천 null 처리
    Former-commit-id: ddce5cfc7f44c7ad827ea2625b4f942453ed0ce3

--- Front/musicnote/src/features/recommend/BookCarousel.tsx
+++ Front/musicnote/src/features/recommend/BookCarousel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e16ac8733aaf9239be21df64827f819b8b8a1c5a12caa5a603f74666d313aeaf
-size 1959
+oid sha256:8c4bf61e5b4774556335d568ae916c763388ff3410e83ba85125b5cf8b4a2dd6
+size 2031
